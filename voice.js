3527a527f1b417b1432326c3445e7f7b5f8decca
Glitch (glitch-hello-node) <none>
2022-08-23 14:24:10 +0000

    🏓🎗 Checkpoint ./voice.js:23817509/51

--- voice.js
+++ voice.js
@@ -268,7 +268,7 @@ twitch.on("message", (channel, userstate, message, self) => {
     usersInQueue[user] = true;
     
     var chatPrompt = "";
-    chatPrompt += `${botName} is a friendly AI dog talking to a human named ${user}. ${botName} responds like a pirate.\n\n`;
+    chatPrompt += `${botName} is a friendly AI dog talking to a human named ${user}. ${botName} talks like a Southern belle.\n\n`;
     chatPrompt += `${user}: ${cleanMessage}\n${botName}:`;
 
     // fakeGenerate(user, message); // for testing only
