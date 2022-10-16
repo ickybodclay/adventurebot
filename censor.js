5d536d1ec63eb05edc026953bf5e50807de80b25
Glitch (glitch-hello-node) <none>
2022-10-16 12:23:28 +0000

    🏐🍅 Checkpoint ./censor.js:23817509/187

--- censor.js
+++ censor.js
@@ -1,8 +1,8 @@
-var censorRegex;
+const censorRegex = /\b(fag|faggot|fags|faggots|nigga|niggas|nigger|niggers|rape|rapes|rapist|rapists|raping)\b/gi
 
 
 function censor(text, replace="*") {
-  
+  return text.replaceAll(censorRegex, replace);
 }
 
 module.exports = { censor };
